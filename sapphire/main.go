0ef48a95b6cfeaddb2f6a07dc0ae5158abefbc21
Brian Hines <brian@projectweekend.net>
2017-07-10 07:06:24 -0500

    That was easy

--- sapphire/main.go
+++ sapphire/main.go
@@ -1,12 +1,36 @@
 package main
 
+
 import (
     "log"
+    "net/http"
+    "net/http/httputil"
 
     "github.com/projectweekend/sapphire/config"
 )
 
+
 func main()  {
     conf := config.Options()
-    log.Println(conf)
+
+    proxyHandler := httputil.NewSingleHostReverseProxy(conf.DstURL)
+
+    http.Handle("/", jwtMiddleware(proxyHandler))
+    http.ListenAndServe(":9009", nil)
+
+}
+
+
+func jwtMiddleware(next http.Handler) http.Handler {
+    handleAuth := func(w http.ResponseWriter, r *http.Request)  {
+        authorization := r.Header.Get("Authorization")
+        if authorization == "" {
+            http.Error(w, http.StatusText(401), 401)
+            return
+        }
+        // TODO: validate and decode JWT token from header
+        log.Println(authorization)
+        next.ServeHTTP(w, r)
+    }
+    return http.HandlerFunc(handleAuth)
 }
